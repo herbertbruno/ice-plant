17e8c68c49fa43b17af51cf64d7d28e6a6588c93
Herbert Bruno <herbertbrunog@gmail.com>
2021-08-22 04:54:55 +0530

    home page url changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'message/:id',
-    loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
+    loadChildren: () => import('./view-message/view-message.module').then(m => m.ViewMessagePageModule)
   },
   {
     path: '',
-    redirectTo: 'sale',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
     path: 'sale',
-    loadChildren: () => import('./sale/sale.module').then( m => m.SalePageModule)
+    loadChildren: () => import('./sale/sale.module').then(m => m.SalePageModule)
   },
 ];
 
