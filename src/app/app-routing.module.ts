7d25afeb9e0c9339252cdb092cc5bd818f52250f
hEzHil <88553060+hEzHil@users.noreply.github.com>
2021-08-28 16:26:21 +0530

    list-customer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -60,6 +60,10 @@ const routes: Routes = [
     path: 'expense',
     loadChildren: () => import('./expense/expense.module').then( m => m.ExpensePageModule)
   },
+  {
+    path: 'list-customer',
+    loadChildren: () => import('./list-customer/list-customer.module').then( m => m.ListCustomerPageModule)
+  },
   
 
 ];
