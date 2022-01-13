503ba9c24fb4f78a20befe9d728fc0fbd9dba00d
hEzHil <88553060+hEzHil@users.noreply.github.com>
2022-01-13 19:35:10 +0530

    creat new sale

--- src/app/pages/list-sale/list-sale.page.ts
+++ src/app/pages/list-sale/list-sale.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ISale } from 'src/app/interfaces/sale';
+import { SaleService } from 'src/app/services/api/sale.service';
 
 @Component({
   selector: 'app-list-sale',
@@ -6,14 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-sale.page.scss'],
 })
 export class ListSalePage implements OnInit {
-  sales = [];
-  
-  constructor() {
+  sales: ISale[];
 
-   } 
-  
+
+  constructor(private saleService: SaleService) { }
+
   ngOnInit() {
-    this.sales = ['Sale 1', 'Sale 2', 'Sale 3', 'Sale 4', 'Sale 5', 'Sale 6'];
+    this.saleService.getSaleList().then(cloudSalesData => {
+      this.sales = cloudSalesData;
+    })
+    // this.sales = ['Sale 1', 'Sale 2', 'Sale 3', 'Sale 4', 'Sale 5', 'Sale 6'];
 
     // for (var sale of this.sales) {
     //    console.log(sale);
