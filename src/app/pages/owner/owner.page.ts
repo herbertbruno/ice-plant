54cb1c3108c8120d7eab0ef8af4cdc1c8e938768
hEzHil <88553060+hEzHil@users.noreply.github.com>
2022-02-02 20:17:32 +0530

    display total ice sold

--- src/app/pages/owner/owner.page.ts
+++ src/app/pages/owner/owner.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ISale } from 'src/app/interfaces/sale';
-import { SaleService } from 'src/app/services/api/sale.service';
+import { CommonService } from 'src/app/services/api/common.service';
 
 @Component({
   selector: 'app-owner',
@@ -10,21 +10,28 @@ import { SaleService } from 'src/app/services/api/sale.service';
 export class OwnerPage implements OnInit {
 
 
-  constructor(private saleService: SaleService) { }
+  constructor(private commonSer: CommonService) { }
   sales: ISale[] = [];
   waitingFlag: boolean;
 
   totalSaleAmount = 0;
+  totalIceSold = 0;
 
   ngOnInit() {
     this.waitingFlag = true;
 
-    this.saleService.getSaleList().then(cloudSalesData => {
+    this.commonSer.getCollectionList('sale').then(cloudSalesData => {
       this.sales = cloudSalesData;
-      for (let i in this.sales) {
-        let sale = this.sales[i];
-        this.totalSaleAmount = this.totalSaleAmount + (sale.ratePerItem * sale.numberOfItems);
-        console.log(sale.ratePerItem + " * " + sale.ratePerItem * sale.numberOfItems)
+   
+      for (let key in this.sales) {
+
+        let sale = this.sales[key];
+        this.totalSaleAmount = this.totalSaleAmount +(sale.ratePerItem * sale.numberOfItems)
+ 
+        this.totalIceSold = this.totalIceSold + ( sale.numberOfItems);
+      
+      
+    console.log(sale.ratePerItem + " * " + sale.numberOfItems + " = " + sale.ratePerItem * sale.numberOfItems )
       }
       this.waitingFlag = false;
     })
