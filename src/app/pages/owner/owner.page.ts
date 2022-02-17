e184c46c99f084fd4aa91f4b85be42484129c529
hEzHil <88553060+hEzHil@users.noreply.github.com>
2022-02-17 22:13:21 +0530

    adding date

--- src/app/pages/owner/owner.page.ts
+++ src/app/pages/owner/owner.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ISale } from 'src/app/interfaces/sale';
-import { IExpense} from 'src/app/interfaces/expense';
+import { IExpense } from 'src/app/interfaces/expense';
 import { CommonService } from 'src/app/services/api/common.service';
 import { ExpenseService } from 'src/app/services/api/expense.service';
 
@@ -10,37 +11,47 @@ import { ExpenseService } from 'src/app/services/api/expense.service';
   styleUrls: ['./owner.page.scss'],
 })
 export class OwnerPage implements OnInit {
-  
+  // ownerform: FormGroup;
 
 
-  constructor(private commonSer: CommonService, private expenseSer:ExpenseService ) { }
+
+  constructor(private commonSer: CommonService, private expenseSer: ExpenseService) { }
   sales: ISale[] = [];
-  waitingFlag: boolean; 
-  totalSaleAmount = 0; 
-  totalSale = 0; 
+  waitingFlag: boolean;
+  totalSaleAmount = 0;
+  totalSale = 0;
   totalIceSold = 0;
-  expenses: IExpense[]  = [];
-  totalExpenseAmount= 0;
+  expenses: IExpense[] = [];
+  totalExpenseAmount = 0;
 
- 
+  selectedDate : any;
+
+  dateChanged(){
+    alert("date changed :: " + this.selectedDate);
+  }
+
   ngOnInit() {
+    
     this.waitingFlag = true;
 
-    this.commonSer.getCollectionList('sale').then((cloudSalesData:any) => {
-      this.sales = cloudSalesData; 
+     
+    this.selectedDate =  new Date();
+
+    this.commonSer.getCollectionList('sale').then((cloudSalesData: any) => {
+      this.sales = cloudSalesData;
       console.log(this.sales)
 
       for (let i in this.sales) {
         let sale = this.sales[i];
         this.totalSaleAmount = this.totalSaleAmount + (sale.ratePerItem * sale.numberOfItems);
-        this.totalSale = this.totalSale + 1;   
-        this.totalIceSold = this.totalIceSold + ( sale.numberOfItems);  
-        console.log(sale.ratePerItem + " * " + sale.numberOfItems + " = " + sale.ratePerItem * sale.numberOfItems ) 
+        this.totalSale = this.totalSale + 1;
+        this.totalIceSold = this.totalIceSold + (sale.numberOfItems);
+        console.log(sale.ratePerItem + " * " + sale.numberOfItems + " = " + sale.ratePerItem * sale.numberOfItems)
       }
       // this.waitingFlag = false;
     })
-    this.expenseSer.getExpenseList().then((cloudExpensesData:any) =>{
-      this.expenses = cloudExpensesData; 
+    this.expenseSer.getExpenseList().then((cloudExpensesData: any) => {
+      this.expenses = cloudExpensesData;
       console.log(this.expenses)
 
       for (let i in this.expenses) {
@@ -48,10 +59,12 @@ export class OwnerPage implements OnInit {
         console.log(expense)
         this.totalExpenseAmount = this.totalExpenseAmount + expense.amount;
 
-    }
-    this.waitingFlag = false;
-  })
+      }
+      this.waitingFlag = false;
+    })
   }
 
 
 }
+
+ 
