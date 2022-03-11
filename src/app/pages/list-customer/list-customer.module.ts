435fabfc2028a52d2115032d6dbf4bba0526fb88
hEzHil <88553060+hEzHil@users.noreply.github.com>
2022-03-11 22:36:17 +0530

    changes in css and impl search function

--- src/app/pages/list-customer/list-customer.module.ts
+++ src/app/pages/list-customer/list-customer.module.ts
@@ -7,12 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { ListCustomerPageRoutingModule } from './list-customer-routing.module';
 
 import { ListCustomerPage } from './list-customer.page';
+import { TranslateModule } from '@ngx-translate/core';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    TranslateModule.forChild(),
+    Ng2SearchPipeModule,
     ListCustomerPageRoutingModule
   ],
   declarations: [ListCustomerPage]
--- src/app/pages/list-customer/list-customer.page.ts
+++ src/app/pages/list-customer/list-customer.page.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 import { ICustomer } from 'src/app/interfaces/customer';
 import { CustomerService } from 'src/app/services/api/customer.service';
+// import { EventEmitter } from 'stream';
+
+
 
 @Component({
   selector: 'app-list-customer',
@@ -9,14 +12,25 @@ import { CustomerService } from 'src/app/services/api/customer.service';
 })
 export class ListCustomerPage implements OnInit {
   customers: ICustomer[];
+  filterTerm: string;
+  
+  
 
-  constructor(private customerService: CustomerService) { }
+  constructor(private customerService: CustomerService) { 
+    
 
-  ngOnInit() {
-    this.customerService.getCustomerList().then(cloudCustomersData => {
-      console.log(cloudCustomersData);
-      this.customers = cloudCustomersData;
-    })
   }
-
+ 
+getCustomerList( event: any = false){
+  this.customerService.getCustomerList().then(cloudCustomersData => {
+    this.customers = cloudCustomersData;
+    if(event){// pull down to refresh call
+      event.detail.complete();// stop loading
+    }
+  })
+}
+  ngOnInit() {
+    this.getCustomerList();
+  }
+ 
 }
